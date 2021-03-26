bcc8c9f1a68c1239f43857a1ee85e0eb4486b997
Cynthia Navarro <cynthianavarro93@hotmail.com>
2021-03-26 16:33:57 -0400

    taco

--- js/index.js
+++ js/index.js
@@ -1,17 +1,26 @@
-function calculateSubTotal() {
-  let price = document.querySelector(".price span").innerText;
-  let quantity = document.querySelector(".quantity input").value;
-  console.log(price);
-  console.log(quantity);
+function calculateSubTotal(product) {
+  let price = product.querySelector(".price span").innerText;
+  let quantity = product.querySelector(".quantity input").value;
+  // console.log(price);
+  // console.log(quantity);
 
   let subTotal = price * quantity;
-  console.log(subTotal)
-  document.querySelector('.subtotal span').innerText = subTotal;
+  // console.log(subTotal)
+  product.querySelector('.subtotal span').innerText = subTotal;
 
   return subTotal
 }
 
+function calculateAll() {
+  let products = document.querySelectorAll("tr.product")
+  for (let p of products) {
+    console.log(p)
+    calculateSubTotal(p)
+  }
+  
+}
+
 document.querySelector('#calculate').onclick = function (e) {
-  calculateSubTotal()
+  calculateAll()
 }
 // document.querySelector('.btn-remove').onclick = function(remove){
