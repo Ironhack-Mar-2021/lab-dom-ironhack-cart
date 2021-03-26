6cd75c132cfa7fe4ad04f7f2bcb88ceb9c210e5d
Bolmey <juan.bolmey@gmail.com>
2021-03-26 16:09:53 -0400

    Group pro

--- js/index.js
+++ js/index.js
@@ -1,42 +1,17 @@
-// ITERATION 1
+function calculateSubTotal() {
+  let price = document.querySelector(".price span").innerText;
+  let quantity = document.querySelector(".quantity input").value;
+  console.log(price);
+  console.log(quantity);
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  let subTotal = price * quantity;
+  console.log(subTotal)
+  document.querySelector('.subtotal span').innerText = subTotal;
 
-  //... your code goes here
+  return subTotal
 }
 
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
-  // ITERATION 2
-  //... your code goes here
-
-  // ITERATION 3
-  //... your code goes here
+document.querySelector('#calculate').onclick = function (e) {
+  calculateSubTotal()
 }
-
-// ITERATION 4
-
-function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
-}
-
-// ITERATION 5
-
-function createProduct() {
-  //... your code goes here
-}
-
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
-});
+// document.querySelector('.btn-remove').onclick = function(remove){
